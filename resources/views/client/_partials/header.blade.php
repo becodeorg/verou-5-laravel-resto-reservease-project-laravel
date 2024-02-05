dc8a6c1f6956142b8ce262cf2bedab866a090f94
Gustave Curtil <gustave.curtil@tutanota.com>
2024-02-05 15:12:42 +0100

    minor change in navigation

--- resources/views/client/_partials/header.blade.php
+++ resources/views/client/_partials/header.blade.php
@@ -5,7 +5,7 @@
             <li><a href="/">Menu</a></li>
             <li><a href="/reservations">Reservations</a></li>
             <li><a href="/about">About</a></li>
-            <li><a href="/employees">Employees</a></li>
         </ul>
     </nav>
+    <a href="/">Back to client side</a>
 </header>
--- resources/views/employees/_partials/header.blade.php
+++ resources/views/employees/_partials/header.blade.php
@@ -3,9 +3,9 @@
     <nav>
         <ul>
             <li><a href="/employees">Calendar</a></li>
-            <li><a href="/employees/tables">tables</a></li>
+            <li><a href="/employees/tables">Tables</a></li>
             <li><a href="/employees/kitchen">Kitchen</a></li>
-            <li><a href="/">Back to client side</a></li>
         </ul>
     </nav>
+    <a href="/">Back to client side</a>
 </header>
