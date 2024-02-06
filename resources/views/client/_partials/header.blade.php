de9dd479016ca7550120ab8a1a4310a5c7a23430
MohammedAB <albay.m@hotmail.com>
2024-02-06 16:17:48 +0100

    Add div section for transition of svg element

--- resources/views/client/_partials/header.blade.php
+++ resources/views/client/_partials/header.blade.php
@@ -1,6 +1,9 @@
 <header>
     <nav>
-        <img src="{{ asset('images/burgerlogowhite.svg') }}">
+         <div class="svg-container" onmouseover="changeSVG('{{ asset('images/burgerman_ogen_toe.svg') }}')" onmouseout="changeSVG('{{ asset('images/burgerlogowhite.svg') }}')">
+         <object type="image/svg+xml" data="{{ asset('images/burgerlogowhite.svg') }}"></object>
+        </div>
+        <!-- <img src="{{ asset('images/burgerlogowhite.svg') }}">  default image element without animation-->
         <a href="/">Menu</a>
         <a href="/reservations">Reservations</a>
         <a href="/about">About</a>
