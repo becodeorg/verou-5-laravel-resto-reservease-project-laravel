c700bfac62b741adfbaa7df3af678a2605fdd5c1
sylvievdebrauwer <sylvie.debrauwer@gmail.com>
2024-02-06 14:03:40 +0100

    finish

--- resources/views/employees/calendar.blade.php
+++ resources/views/employees/calendar.blade.php
@@ -13,18 +13,18 @@
     
     @else
     
-    @include('users._partials.header')
+    @include('employees._partials.header')
 
     <main>
         <h2>Login</h2>
-        <form action="/users/login" method="POST">
+        <form action="/employees/login" method="POST">
             @csrf
             <input type="text" name="name" placeholder="Name"><br>
             <input type="password" name="password" placeholder="Password"><br>
             <button>Login</button>
         </form>
         <h2>Register</h2>
-        <form action="/users/register" method="POST">
+        <form action="/employees/register" method="POST">
             @csrf
             <input type="text" name="name" placeholder="Name"><br>
             <input type="text" name="email" placeholder="Email"><br>
