80f2972b47fd99662e3a6104486af2538da2623c
sylvievdebrauwer <sylvie.debrauwer@gmail.com>
2024-02-06 16:56:12 +0100

    momo's function deleted

--- resources/views/client/_partials/_head.blade.php
+++ resources/views/client/_partials/_head.blade.php
@@ -7,10 +7,4 @@
     <title>Reserve'Ease</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200&display=swap" rel="stylesheet">
-    <script>
-        function changeSVG(newSVG) {
-            var objectElement = document.querySelector('.svg-container object');
-            objectElement.setAttribute('data', newSVG);
-        }
-    </script>
 </head>
