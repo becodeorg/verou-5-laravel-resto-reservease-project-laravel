f87d124845e090981b72e4c1caf6539137b4f520
MohammedAB <albay.m@hotmail.com>
2024-02-06 12:18:04 +0100

    Create orders migration file

---
+++ database/migrations/2024_02_06_111312_create_orders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('table_id')->references('id')->on('tables');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('employee_id')->references('id')->on('employees');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
