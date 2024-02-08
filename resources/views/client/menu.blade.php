1253b2a8f5bb3ad2f4f83eb4493d27420a2fbc21
sylvievdebrauwer <sylvie.debrauwer@gmail.com>
2024-02-08 15:39:42 +0100

    small

--- resources/views/client/menu.blade.php
+++ resources/views/client/menu.blade.php
@@ -9,7 +9,36 @@
     <main>
         <h2>Menu</h2>
 
-    
+        <div class="container left-container">
+            <h2>Menu Categories</h2>
+            <ul class="menu-options">
+                <li><a href="#">Breakfast</a></li>
+                <li><a href="#">Lunch</a></li>
+                <li><a href="#">Soup</a></li>
+                <li><a href="#">Salad</a></li>
+                <li><a href="#">Pasta</a></li>
+                <li><a href="#">Kids Menu</a></li>
+                <li><a href="#">Snacks</a></li>
+                <li><a href="#">Desserts</a></li>
+                <li><a href="#">Beverages</a></li>
+            </ul>
+        </div>
+
+
+        <div class="container middle-container">
+            <h2>Menu Items</h2>
+            <div id="menu-items">
+                <!-- Menu items will be displayed here based on selection -->
+            </div>
+        </div>
+
+
+        <div class="container right-container">
+            <img class="menu-image" src="your_image_url.jpg" alt="Menu Image">
+        </div>
+
+
+        
         <article>
             <h1><a href="/item">Appetizers</a></h1>
             <p>
