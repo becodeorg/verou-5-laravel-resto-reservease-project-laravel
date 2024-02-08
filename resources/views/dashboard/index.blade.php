b4253819377f0e0bd51048f611fb663c3c4430e0
MohammedAB <albay.m@hotmail.com>
2024-02-08 23:30:48 +0100

    Create index page for dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,9 +1,17 @@
 @extends('_layouts.layout')
 
 @section('title')
-Dashboard Page
+Main dashboard
 @endsection
 
-@section
-
+@section('content')
+<main class="container mx-auto w-4/5 ">
+  @auth
+    <h1>Welcome back, {{ auth()->user()->name }}!</h1>
+  @endauth
+  <div>
+    <a href="{{ route('showReservations') }}">See Bookings</a>
+    <a href="{{ route('editKitchen') }}">Edit menu content</a>
+  </div>
+</main>
 @endsection
