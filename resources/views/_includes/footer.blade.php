d0b0fd8a57808b7355d199c3799cf07df01769da
MohammedAB <albay.m@hotmail.com>
2024-02-08 14:25:58 +0100

    Create footer partial

---
+++ resources/views/_includes/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+    <p class="footer-text">This project is the intellectual property of the Ziekenboeg Devs Department of Becode Ghent &copy; {{ Date ('Y') }}. 
+       This website is for educational purposes only.
+    </p>
+</footer>
